26fcf74ee563099d7c9bd65a6779f479c8db71c1
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-19 18:39:05 +0300

    style(symbol): remove

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation } from 'next-i18next'
 
 import { ThemeProvider } from 'styled-components'
 import { mainTheme as theme } from 'danni-s-design-system'
