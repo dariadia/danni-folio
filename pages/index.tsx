53addc3d4f11f0917e6fbde6046407eb251b7693
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-19 15:25:37 +0300

    feat(app): wrap in translations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
 import styled from 'styled-components'
-import type { Page } from 'types'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import type { Page, Locale } from 'types'
 
 const Title = styled('h1')`
   font-size: 50px;
   color: ${({ theme }) => theme.colours.accentDark};
 `
 
-const HomePage: Page<{}> = () => {
+type Props = {
+
+}
+
+const HomePage: Page<Props> = () => {
   return <Title>Hello world</Title>
+}
+
+export async function getStaticProps({ locale }: {locale: Locale}): Promise<{ props: Props }> {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  }
 }
 
 export default HomePage
