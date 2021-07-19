1470b013c2d1e797c943be3d38a33e1f30f487fa
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-19 20:14:31 +0300

    feat(index): add language switch sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
 import styled from 'styled-components'
+
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+
+import { Box } from 'danni-s-design-system'
 
 import type { Page, Locale } from 'types'
 
@@ -14,7 +19,26 @@ type Props = {}
 const HomePage: Page<Props> = () => {
   const { t } = useTranslation('common')
 
-  return <Title>{t('greeting')}</Title>
+  return (
+    <>
+      <Link href="/" locale="en-GB">
+        <a>en-GB</a>
+      </Link>
+      <Box mr="s" inlineBlock />
+      <Link href="/" locale="en-US">
+        <a>en-US</a>
+      </Link>
+      <Box mr="s" inlineBlock />
+      <Link href="/" locale="de">
+        <a>DE</a>
+      </Link>
+      <Box mr="s" inlineBlock />
+      <Link href="/" locale="ru">
+        <a>RU</a>
+      </Link>
+      <Title>{t('greeting')}</Title>
+    </>
+  )
 }
 
 export async function getStaticProps({
