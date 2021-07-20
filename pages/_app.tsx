f2b381e13cc596aac945382892ef5bc06843f22f
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-20 19:06:40 +0300

    feat(app): wrap in layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ComponentType } from 'react'
 import { AppProps } from 'next/app'
 
 import { appWithTranslation } from 'next-i18next'
@@ -6,11 +6,19 @@ import { appWithTranslation } from 'next-i18next'
 import { ThemeProvider } from 'styled-components'
 import { mainTheme as theme } from 'danni-s-design-system'
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+type ApplicationProps = AppProps & {
+  Component: AppProps['Component'] & { Layout?: React.FC }
+}
+
+const App: React.FC<ApplicationProps> = ({ Component, pageProps }) => {
+  const Layout: ComponentType = Component.Layout || React.Fragment
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Layout {...pageProps}>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </>
   )
