f96dd7ba84dee6e99a28bf9337da23532f859ada
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-25 15:11:40 +0300

    feat(SelfAvatar): add sample layout

---
+++ src/components/SelfAvatar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Circle } from 'danni-s-design-system'
+
+const NONBINARY_COLOUR =
+  'linear-gradient(180deg, #FFF430 25%, #FFFFFF 25%, 50%, #9C59D1 50%, 75%, #181818 75%)'
+const GREYROMANTIC_COLOUR =
+  'linear-gradient(180deg, #39A33E 20%, #A3A3A3 20%, 40%, #FFFFFF 40%, 60%, #A3A3A3 60%, 80%, #39A33E 80%)'
+
+const Wrapper = styled(Circle).attrs({
+  size: 'elephant',
+})`
+  background: ${NONBINARY_COLOUR};
+  overflow: hidden;
+  transition: background 0.5s ease;
+  &:hover {
+    background: ${GREYROMANTIC_COLOUR};
+    .shine::before {
+      animation: glasses-flash 2s ease-in reverse;
+    }
+`
+
+export const SelfAvatar: React.FC = () => <Wrapper />
