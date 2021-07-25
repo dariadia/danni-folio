581429890716cda48b015dbc03d848b5567c3a1a
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-25 15:08:30 +0300

    feat(layout): add mobile option

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,38 +1,57 @@
 import React from 'react'
 import { useRouter } from 'next/dist/client/router'
 
+import { useUserAgent } from 'next-useragent'
+import { isDeviceDesktop } from 'utils/device'
+
 import { Grid, Box, baseTheme } from 'danni-s-design-system'
 import { Header, Footer } from '.'
 
 import { Layout } from 'types'
 
-const getGridSx = (isIndexPage: boolean) => {
+const getGridSx = ({
+  isDesktop,
+  isIndexPage,
+}: {
+  isDesktop: boolean
+  isIndexPage: boolean
+}) => {
   const baseProps = {
     maxHeight: `calc((100% - ${baseTheme.space.xxl}px))`,
     minHeight: '100vh',
     boxShadow: baseTheme.shadows.bookLight,
   }
+  const BOOK_TWO_PAGES_WIDTH = `calc(100% - ${baseTheme.space.xxl}px)`
+  const BOOK_ONE_PAGE_WIDTH = `calc((100% - ${baseTheme.space.xxl}px) / 2)`
+
   return isIndexPage
     ? {
         ...baseProps,
-        maxWidth: `calc((100% - ${baseTheme.space.xxl}px) / 2)`,
+        maxWidth: isDesktop ? BOOK_ONE_PAGE_WIDTH : BOOK_TWO_PAGES_WIDTH,
         gridTemplateColumns: `repeat(1, 1fr)`,
       }
     : {
         ...baseProps,
-        maxWidth: `calc(100% - ${baseTheme.space.xxl}px)`,
+        maxWidth: BOOK_TWO_PAGES_WIDTH,
         gridTemplateColumns: `repeat(2, 1fr)`,
       }
 }
 
-export const MainLayout: React.FC<Layout> = ({ children }) => {
+export const MainLayout: React.FC<Layout> = ({ children, userAgentString }) => {
   const router = useRouter()
   const isIndexPage = router.route === '/'
+  const userAgent = useUserAgent(userAgentString || window.navigator.userAgent)
+  const isDesktop = isDeviceDesktop(userAgent)
 
   return (
     <Box bg="darkest">
       <Header />
-      <Grid m="auto" p="l" bg="white" sx={getGridSx(isIndexPage)}>
+      <Grid
+        m="auto"
+        p="l"
+        bg="white"
+        sx={getGridSx({ isIndexPage, isDesktop })}
+      >
         {children}
       </Grid>
       <Footer />
