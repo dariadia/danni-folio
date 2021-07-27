b2b1916afcee421ee398acc3544424aa70bbf634
Daria Diachkova <daria.diachkova@mail.ru>
2021-07-27 11:56:41 +0300

    feat(SelfAvatar): add hair top

--- src/components/SelfAvatar.tsx
+++ src/components/SelfAvatar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { darken } from 'polished'
 
 import { Circle, Box, baseTheme, mainTheme } from 'danni-s-design-system'
 import type { ConstrainedBoxProps } from 'danni-s-design-system'
@@ -49,7 +50,7 @@ const HairBack = styled(Box).attrs({
     width: 107px;
     height: 80px;
     background: ${HAIR};
-    box-shadow: inset 2px -25px 24px 0px darken($hair, 15%);
+    box-shadow: inset 2px -25px 24px 0px ${darken(0.15, HAIR)};
     border-radius: 300px;
     top: 36%;
     right: -8%;
@@ -67,7 +68,7 @@ const Shirt = styled(Box)`
   &:after {
     content: '';
     position: absolute;
-    border-bottom: 20px solid darken(white, 10%);
+    border-bottom: 20px solid ${darken(0.1, 'white')};
     border-left: 20x solid transparent;
     border-right: 15px solid transparent;
     height: 0;
@@ -78,7 +79,7 @@ const Shirt = styled(Box)`
   &:before {
     content: '';
     position: absolute;
-    border-bottom: 20px solid darken(white, 20%);
+    border-bottom: 20px solid ${darken(0.2, 'white')};
     border-left: 15px solid transparent;
     border-right: 0px solid transparent;
     height: 0;
@@ -98,10 +99,34 @@ const ShirtButton = styled(Box)`
   top: 10px;
 `
 
+const HairTop = styled(Circle)`
+  position: absolute;
+  width: 130px;
+  height: 130px;
+  background: ${HAIR};
+  left: calc(50% - 65px);
+  top: 12%;
+  box-shadow: inset 10px 7px 5px 0px ${darken(0.07, HAIR)};
+  &:after {
+    content: '';
+    position: absolute;
+    width: 90px;
+    height: 40px;
+    background: ${HAIR};
+    border-radius: 0px 200px 0px 200px;
+    z-index: 4;
+    left: 40px;
+    top: 20px;
+    transform: rotate(10deg);
+    box-shadow: inset 1px 12px 9px 6px ${darken(0.07, HAIR)};
+  }
+`
+
 export const SelfAvatar: React.FC<ConstrainedBoxProps> = ({ mx, my }) => (
   <Wrapper {...{ mx, my }}>
     <HairBack side={LEFT} />
     <HairBack side={RIGHT} />
+    <HairTop />
     <Shirt>
       <ShirtButton />
     </Shirt>
