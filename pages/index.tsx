37bf77c0c2349309990f8a84d2ff140bf0530426
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-05 14:38:40 +0300

    feat(index): add sample motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
+import { NextApiRequest } from 'next'
 
+import { motion } from 'framer-motion'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import Link from 'next/link'
 import { MainLayout } from '@/components/layouts'
 import { SelfAvatar } from '@/components'
 
 import type { Page, Locale, IndexPage as IndexPageProps } from 'types'
-import { NextApiRequest } from 'next'
+import { Button } from 'danni-s-design-system'
 
 const HomePage: Page<IndexPageProps> = () => {
   return (
     <>
-      <SelfAvatar mx="auto" my="xxxl" />
+      <motion.div className="selfAvatar" layoutId="selfAvatar">
+        <SelfAvatar mx="auto" my="xxxl" />
+      </motion.div>
+      <motion.div
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.4 }}
+      >
+        <Link href="/contents" passHref>
+          <Button
+            activeColor="complementaryDark"
+            bg="accentDark"
+            color="accentLightest"
+            p="s"
+            my="l"
+            mx="auto"
+            textAlign="center"
+            transition="slow"
+          >
+            Open!
+          </Button>
+        </Link>
+      </motion.div>
     </>
   )
 }
