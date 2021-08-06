42cb8f33f113c3109206a3656b719a3a2615ae94
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-06 17:34:48 +0300

    fix(contents-page): layout

--- pages/contents/[[...page]].tsx
+++ pages/contents/[[...page]].tsx
@@ -3,8 +3,8 @@ import { NextApiRequest } from 'next'
 
 import { motion } from 'framer-motion'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-import { Media, MediaContextProvider } from 'utils/media'
+import { useUserAgent } from 'next-useragent'
+import { isDeviceDesktop } from 'utils/device'
 
 import Link from 'next/link'
 import { baseTheme } from 'danni-s-design-system'
@@ -14,53 +14,53 @@ import { GoToMainButton, SelfAvatar } from '@/components'
 
 import type { Page, Locale, ContentsPage as ContentsPageProps } from 'types'
 
-const ContentsPage: Page<ContentsPageProps> = () => {
+const ContentsPage: Page<ContentsPageProps> = ({ userAgentString }) => {
+  const userAgent = useUserAgent(userAgentString || window.navigator.userAgent)
+  const isDesktop = isDeviceDesktop(userAgent)
+
   const moveSelfAvatarX = -baseTheme.space.elephant - baseTheme.space.xl
   const moveSelfAvatarY = -baseTheme.space.elephant * 2 + baseTheme.space.l
 
   const moveButtonX = baseTheme.space.elephant
   const moveButtonY = -baseTheme.space.xxl
 
-  return (
-    <MediaContextProvider>
-      <Media greaterThanOrEqual="tablet">
-        <Link href="/" passHref>
-          <motion.div
-            className="selfAvatar"
-            layoutId="selfAvatar"
-            style={{ cursor: 'pointer' }}
-            initial={{
-              scale: 0.4,
-              x: moveSelfAvatarX,
-              y: moveSelfAvatarY,
-            }}
-          >
-            <SelfAvatar />
-          </motion.div>
-        </Link>
-        <Link href="/" passHref>
-          <motion.a
-            style={{ textDecoration: 'none', height: 'fit-content' }}
-            layoutId="navButton"
-            initial={{ scale: 0.47, x: moveButtonX, y: moveButtonY }}
-            whileHover={{ scale: 0.5 }}
-            whileTap={{ scale: 0.5 }}
-          >
-            <GoToMainButton />
-          </motion.a>
-        </Link>
-      </Media>
-      <Media lessThan="tablet">
-        <Link href="/" passHref>
-          <motion.a
-            style={{ textDecoration: 'none', height: 'fit-content' }}
-            layoutId="navButton"
-          >
-            <GoToMainButton />
-          </motion.a>
-        </Link>
-      </Media>
-    </MediaContextProvider>
+  return isDesktop ? (
+    <>
+      <Link href="/" passHref>
+        <motion.div
+          className="selfAvatar"
+          layoutId="selfAvatar"
+          style={{ cursor: 'pointer' }}
+          initial={{
+            scale: 0.4,
+            x: moveSelfAvatarX,
+            y: moveSelfAvatarY,
+          }}
+        >
+          <SelfAvatar />
+        </motion.div>
+      </Link>
+      <Link href="/" passHref>
+        <motion.a
+          style={{ textDecoration: 'none', height: 'fit-content' }}
+          layoutId="navButton"
+          initial={{ scale: 0.47, x: moveButtonX, y: moveButtonY }}
+          whileHover={{ scale: 0.5 }}
+          whileTap={{ scale: 0.5 }}
+        >
+          <GoToMainButton />
+        </motion.a>
+      </Link>
+    </>
+  ) : (
+    <Link href="/" passHref>
+      <motion.a
+        style={{ textDecoration: 'none', height: 'fit-content' }}
+        layoutId="navButton"
+      >
+        <GoToMainButton />
+      </motion.a>
+    </Link>
   )
 }
 
