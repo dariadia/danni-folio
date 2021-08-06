4ea2a03e6d692392593943b3bbf44eaf5939e6f0
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-06 14:40:42 +0300

    feat(page): move avatar

--- pages/contents/[[...page]].tsx
+++ pages/contents/[[...page]].tsx
@@ -9,16 +9,24 @@ import { SelfAvatar } from '@/components'
 import { motion } from 'framer-motion'
 
 import type { Page, Locale, ContentsPage as ContentsPageProps } from 'types'
+import { baseTheme } from 'danni-s-design-system'
 
 const ContentsPage: Page<ContentsPageProps> = () => {
+  const moveX = -baseTheme.space.elephant - baseTheme.space.xl
+  const moveY = -baseTheme.space.elephant
+
   return (
     <>
       <motion.div
         className="selfAvatar"
         layoutId="selfAvatar"
-        initial={{ scale: 0.5 }}
+        initial={{
+          scale: 0.4,
+          x: moveX,
+          y: moveY,
+        }}
       >
-        <SelfAvatar mx="auto" my="xxxl" />
+        <SelfAvatar />
       </motion.div>
     </>
   )
