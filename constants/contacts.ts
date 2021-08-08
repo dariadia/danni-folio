f33c56a5115955e7fb14c48a8b0901b26188521e
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-08 17:16:47 +0300

    feat(consts): add contacts

---
+++ constants/contacts.ts
@@ -0,0 +1,9 @@
+export const CONTACTS = {
+  GITHUB: 'https://github.com/dariadia/',
+  TELEGRAM: '@dariadia',
+  PHONE_NUMBER: '+7 917 168 1854',
+  EMAIL: 'mailto:daria.diachkova@mail.ru',
+  PORTFOLIO: 'https://danni-s-folio.vercel.app/',
+  VK: 'https://vk.com/dariadia/',
+  LINKEDIN: 'https://www.linkedin.com/in/dariadiachkova/',
+} as const
