de0195063b4e188a5d66699c7d7808b12af3104a
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-08 21:39:28 +0300

    feat(contacts-page): position items mobile

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from 'styled-components'
 
+import { motion } from 'framer-motion'
+
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { CONTACTS } from 'constants/contacts'
+import { MediaContextProvider, Media } from 'utils/media'
 
 import { MainLayout } from '@/components/layouts'
 import {
@@ -16,6 +19,7 @@ import {
   Text,
   HoverableText,
   Link,
+  HeadingH3,
 } from 'danni-s-design-system'
 
 import type { Locale, Page, SinglePage as SinglePageProps } from 'types'
@@ -74,6 +78,8 @@ const Contacts = () => {
 const ContactsPage: Page<SinglePageProps> = () => {
   const { t } = useTranslation(['contacts'])
   const theme = useContext(ThemeContext) as ThemeType
+  const delayHeading = baseTheme.durations.s / 1000
+  const delayContents = baseTheme.durations.l / 1000
 
   return (
     <>
@@ -87,24 +93,51 @@ const ContactsPage: Page<SinglePageProps> = () => {
           boxShadow: baseTheme.shadows.low,
         }}
       >
-        <HeadingH2
-          as="h1"
-          kind="serif"
-          m="auto"
-          px="l"
-          color="accentDark"
-          width="fit-content"
-          sx={{
-            textTransform: 'capitalize',
-            borderBottom: `2px solid ${theme.colours.complementaryDark}`,
-          }}
+        <motion.div
+          className="contactsHeading"
+          layoutId="contactsHeading"
+          style={{ width: 'fit-content', margin: 'auto' }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: delayHeading }}
         >
-          {t('contacts')}
-        </HeadingH2>
+          <MediaContextProvider>
+            <Media greaterThanOrEqual="tablet">
+              <HeadingH2
+                as="h1"
+                kind="serif"
+                m="auto"
+                px="l"
+                color="accentDark"
+                width="fit-content"
+                sx={{
+                  textTransform: 'capitalize',
+                  borderBottom: `2px solid ${theme.colours.complementaryDark}`,
+                }}
+              >
+                {t('contacts')}
+              </HeadingH2>
+            </Media>
+            <Media lessThan="tablet">
+              <HeadingH3
+                as="h1"
+                kind="serif"
+                ml="m"
+                px="s"
+                color="accentDark"
+                sx={{
+                  textTransform: 'capitalize',
+                  borderBottom: `2px solid ${theme.colours.complementaryDark}`,
+                }}
+              >
+                {t('contacts')}
+              </HeadingH3>
+            </Media>
+          </MediaContextProvider>{' '}
+        </motion.div>
       </Flex>
       <Flex
         as="section"
-        pl="xl"
         pr="s"
         py="s"
         sx={{
@@ -113,9 +146,16 @@ const ContactsPage: Page<SinglePageProps> = () => {
           boxShadow: baseTheme.shadows.low,
         }}
       >
-        <List sx={{ margin: 'auto' }} width="fit-content">
-          {Contacts()}
-        </List>
+        <motion.div
+          className="contactsContent"
+          layoutId="contactsContent"
+          style={{ margin: 'auto', width: 'fit-content' }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, x: baseTheme.space.s }}
+          transition={{ delay: delayContents }}
+        >
+          <List>{Contacts()}</List>
+        </motion.div>
       </Flex>
     </>
   )
