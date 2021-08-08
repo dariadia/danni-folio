fe06ef6bcce69f573c7080be60b13303583e4d58
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-08 19:49:04 +0300

    feat(consts): add contacts

--- constants/contacts.ts
+++ constants/contacts.ts
@@ -1,9 +1,26 @@
-export const CONTACTS = {
-  GITHUB: 'https://github.com/dariadia/',
-  TELEGRAM: '@dariadia',
-  PHONE_NUMBER: '+7 917 168 1854',
-  EMAIL: 'mailto:daria.diachkova@mail.ru',
-  PORTFOLIO: 'https://danni-s-folio.vercel.app/',
-  VK: 'https://vk.com/dariadia/',
-  LINKEDIN: 'https://www.linkedin.com/in/dariadiachkova/',
+import { Contacts } from 'types'
+
+export const CONTACTS: Contacts = {
+  GITHUB: { link: 'https://github.com/dariadia/', value: 'dariadia' },
+  TELEGRAM: { value: '@dariadia' },
+  PHONE_NUMBER: { translationKey: 'phone_number', value: '+7 917 168 1854' },
+  EMAIL: {
+    translationKey: 'email',
+    link: 'mailto:daria.diachkova@mail.ru',
+    value: 'daria.diachkova@mail.ru',
+  },
+  PORTFOLIO: {
+    link: 'https://danni-s-folio.vercel.app/',
+    value: `Danni's Folio`,
+    hintKey: 'you_are_here',
+  },
+  INSTAGRAM: {
+    link: 'https://www.instagram.com/_dariadia/',
+    value: '_dariadia',
+  },
+  VK: { link: 'https://vk.com/dariadia/', value: 'dariadia' },
+  LINKEDIN: {
+    link: 'https://www.linkedin.com/in/dariadiachkova/',
+    value: 'dariadiachkova',
+  },
 } as const
