0340efb51ef1419066954ca65e826738f75b3f14
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-08 16:33:07 +0300

    feat(contacts): add heading

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,18 +1,59 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { MainLayout } from '@/components/layouts'
 
+import { baseTheme, Flex, HeadingH2, ThemeType } from 'danni-s-design-system'
+
 import type { Locale, Page, SinglePage as SinglePageProps } from 'types'
+import { ThemeContext } from 'styled-components'
 
 const ContactsPage: Page<SinglePageProps> = () => {
-  const { t } = useTranslation(['common'])
+  const { t } = useTranslation(['contacts'])
+  const theme = useContext(ThemeContext) as ThemeType
 
   return (
     <>
-      <div>{t('look_around')}</div>
+      <Flex
+        pl="s"
+        pr="xxl"
+        py="m"
+        sx={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          boxShadow: baseTheme.shadows.low,
+        }}
+      >
+        <HeadingH2
+          as="h1"
+          kind="serif"
+          m="auto"
+          px="l"
+          color="accentDark"
+          width="fit-content"
+          sx={{
+            textTransform: 'capitalize',
+            borderBottom: `2px solid ${theme.colours.complementaryDark}`,
+          }}
+        >
+          {t('contacts')}
+        </HeadingH2>
+      </Flex>
+      <Flex
+        as="section"
+        pl="xl"
+        pr="s"
+        py="s"
+        sx={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          boxShadow: baseTheme.shadows.low,
+        }}
+      >
+        {t('contacts')}
+      </Flex>
     </>
   )
 }
@@ -29,7 +70,7 @@ export async function getStaticProps({
   return {
     props: {
       locale,
-      ...(await serverSideTranslations(locale, ['common'])),
+      ...(await serverSideTranslations(locale, ['common', 'contacts'])),
     },
   }
 }
