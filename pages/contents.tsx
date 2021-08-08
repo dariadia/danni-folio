d76351f5d6eeab38172cf5aa4fe5ac60dac309b0
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-08 10:01:58 +0300

    feat(contents): add mobile version

--- pages/contents.tsx
+++ pages/contents.tsx
@@ -112,7 +112,7 @@ const Greeting = () => {
         </Box>
       </Media>
       <Media lessThan="tablet">
-        <Box mt="dinosaur" as="section">
+        <Box mt="dinosaur" as="section" textAlign="center">
           <HeadingH3 as="h1" kind="serif">
             {t('greeting')}
           </HeadingH3>
@@ -128,14 +128,12 @@ const Greeting = () => {
   )
 }
 
-const ContentsPage: Page<SinglePageProps> = () => {
+const IntroSection = () => {
   const { t } = useTranslation(['introduction'])
 
   return (
-    <>
-      <Box px="s" py="m" sx={{ boxShadow: baseTheme.shadows.low }}>
-        <SelfAvatarWithMotion />
-        <Greeting />
+    <MediaContextProvider>
+      <Media greaterThanOrEqual="tablet">
         <Box px="s" mt="xxxl" as="section">
           <HeadingH3 sx={{ fontSize: baseTheme.space.xl }}>
             <Trans
@@ -182,31 +180,89 @@ const ContentsPage: Page<SinglePageProps> = () => {
             </Text>
           </List>
         </Box>
-      </Box>
-      <Flex
-        as="section"
-        pl="xl"
-        pr="s"
-        py="s"
-        flexDirection="column"
-        justifyContent="center"
-        sx={{ boxShadow: baseTheme.shadows.low, textAlign: 'center' }}
-      >
-        <HeadingH3 as="h2" my="m" color="complementaryDark">
-          Contents
-        </HeadingH3>
-        <List liSx={{ margin: `${baseTheme.space.m}px` }}>
-          <Link href="#" passHref>
-            <HoverableText variant="bodyMd">Some page........ 1</HoverableText>
+      </Media>
+      <Media lessThan="tablet">
+        <Box px="s" mt="xxxl" as="section">
+          <HeadingH3 sx={{ fontSize: baseTheme.space.xl }}>
+            <Trans
+              i18nKey="introduction:intro_heading"
+              components={{ italic: <i /> }}
+            />
+          </HeadingH3>
+          <Text my="m" as="p">
+            {t('folio')}
+          </Text>
+          <List liSx={{ marginLeft: `${baseTheme.space.l}px` }}>
+            <Text mt="s" variant="bodySm">
+              {t('folio_definition_one')}
+            </Text>
+            <Text mt="s" variant="bodySm">
+              {t('folio_definition_two')}
+            </Text>
+          </List>
+          <Link href={t('folio_link')} passHref>
+            <HoverableText my="m" ml="l" variant="bodySm">
+              {t('definition_origin')}
+            </HoverableText>
           </Link>
-          <Link href="#" passHref>
-            <HoverableText variant="bodyMd">Some page......... 2</HoverableText>
-          </Link>
-        </List>
-      </Flex>
-    </>
+          <Text mt="l" mb="m" as="p" fontWeight="bold">
+            {t('folio_includes')}
+          </Text>
+          <List liSx={{ marginLeft: `${baseTheme.space.l}px` }}>
+            <Text mt="s">{t('folio_item_books')}</Text>
+            <Text mt="s">{t('folio_item_code')}</Text>
+            <Text mt="s">
+              <Trans
+                i18nKey="introduction:folio_item_colour"
+                components={{
+                  green: (
+                    <Text
+                      color="complementaryDark"
+                      fontWeight="bold"
+                      as="span"
+                      inlineBlock
+                    />
+                  ),
+                }}
+              />
+            </Text>
+          </List>
+        </Box>
+      </Media>
+    </MediaContextProvider>
   )
 }
+
+const ContentsPage: Page<SinglePageProps> = () => (
+  <>
+    <Box px="s" py="m" sx={{ boxShadow: baseTheme.shadows.low }}>
+      <SelfAvatarWithMotion />
+      <Greeting />
+      <IntroSection />
+    </Box>
+    <Flex
+      as="section"
+      pl="xl"
+      pr="s"
+      py="s"
+      flexDirection="column"
+      justifyContent="center"
+      sx={{ boxShadow: baseTheme.shadows.low, textAlign: 'center' }}
+    >
+      <HeadingH3 as="h2" my="m" color="complementaryDark">
+        Contents
+      </HeadingH3>
+      <List liSx={{ margin: `${baseTheme.space.m}px` }}>
+        <Link href="#" passHref>
+          <HoverableText variant="bodyMd">Some page........ 1</HoverableText>
+        </Link>
+        <Link href="#" passHref>
+          <HoverableText variant="bodyMd">Some page......... 2</HoverableText>
+        </Link>
+      </List>
+    </Flex>
+  </>
+)
 
 ContentsPage.Layout = ({ children, ...props }) => (
   <MainLayout {...props}>{children}</MainLayout>
