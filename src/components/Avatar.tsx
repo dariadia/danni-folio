237b10a273815a6370493a48af62b00e9a7616cd
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-09 19:06:43 +0300

    feat(components): add Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { Circle } from 'danni-s-design-system'
+import { Circle, CircleProps } from 'danni-s-design-system'
 
-export const Avatar: React.FC = styled(Circle)`
+export const Avatar: React.FC<CircleProps> = styled(Circle)`
   overflow: hidden;
   user-select: none;
   & > img {
