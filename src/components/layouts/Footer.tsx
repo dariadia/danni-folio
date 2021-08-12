7a72ccbb332b60327e3c9333781394ace9283762
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-12 21:23:08 +0300

    feat(Footer): add plans

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,29 +1,46 @@
 import React from 'react'
+import { useTranslation } from 'next-i18next'
 
-import { HoverableText, Text, Link } from 'danni-s-design-system'
+import { HoverableText, Text, Link, Flex } from 'danni-s-design-system'
 
 import { CONTACTS } from 'constants/contacts'
 
 const TTO = 2021
 
 export const Footer: React.FC = () => {
+  const { t } = useTranslation(['common'])
+
   const currentYear = new Date().getFullYear()
 
   return (
-    <Text mr="xxl" sx={{ direction: 'rtl' }}>
-      <Text color="accentLightest" ml="s" inlineBlock>
-        2021 {currentYear !== TTO && `– ${currentYear}`}
+    <Flex mx="xl" p="m" justifyContent="flex-end">
+      <Text>
+        <Link
+          target="_blank"
+          href={CONTACTS.GITHUB.link}
+          color="accentLightest"
+          sx={{ textDecoration: 'none' }}
+          inlineBlock
+        >
+          <HoverableText activeColour="complementaryDark">
+            {CONTACTS.GITHUB.value} ©
+          </HoverableText>
+        </Link>
+        <Text color="accentLightest" ml="s" inlineBlock>
+          2021{currentYear !== TTO && ` – ${currentYear}`}.
+        </Text>
       </Text>
       <Link
+        href="https://github.com/dariadia/danni-s-folio/issues"
+        target="_blank"
         color="accentLightest"
-        href={CONTACTS.GITHUB.link}
         sx={{ textDecoration: 'none' }}
-        inlineBlock
+        ml="s"
       >
         <HoverableText activeColour="complementaryDark">
-          © {CONTACTS.GITHUB.value}
+          {t('plans')}
         </HoverableText>
       </Link>
-    </Text>
+    </Flex>
   )
 }
