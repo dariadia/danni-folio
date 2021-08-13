db98463a2b3131d03066296d65f1d38f087dc4af
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-13 22:20:37 +0300

    feat(types): add Request

---
+++ types/Request.d.ts
@@ -0,0 +1,12 @@
+import { Locale } from './'
+import { Fetcher, Configuration } from 'swr/dist/types'
+
+export interface RequestOptions {
+  page?: number
+  per_page?: number
+  locale?: Locale
+}
+
+export type RequestOption = keyof RequestOptions
+
+export type OptionsType = Partial<Configuration<unknown, any, Fetcher<unknown>>>
