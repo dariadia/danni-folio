377bcf8c1236a21410fb8b73016c08a0504cdb71
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-17 17:29:48 +0300

    feat(project): extend story maker

--- pages/projects/random-story-maker.tsx
+++ pages/projects/random-story-maker.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
-// import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { MainLayout } from '@/components/layouts'
+import { HeadingH3, Text, baseTheme, Box } from 'danni-s-design-system'
 
 import type { Locale, Page, SinglePage as SinglePageProps } from 'types'
 
-const RandomStoryPage: Page<SinglePageProps> = () => {
-  // const { t } = useTranslation(['common'])
+const RandomStoryPage: Page<SinglePageProps> = () => (
+  <>
+    <Box my="xl" as="section" textAlign="center">
+      <HeadingH3 as="h1" kind="serif">
+        🧑🏻‍🦰 Enter your name to get a story 👨🏾‍🦰
+      </HeadingH3>
+      <HeadingH3
+        color="complementaryDark"
+        mt="xs"
+        as="h2"
+        fontSize={`${baseTheme.space.xl}px`}
+      >
+        about you going on an epic adventure with an author
+      </HeadingH3>
+      <Text mt="xs">or enter any text you want and take off 🚀</Text>
+    </Box>
 
-  return <>hello world</>
-}
+    <iframe
+      suppressHydrationWarning
+      height="300"
+      style={{ width: '100%' }}
+      scrolling="no"
+      title="Plot Generator"
+      src="https://codepen.io/dariadia/embed/bGWEzvP?default-tab=result"
+      frameBorder="no"
+      loading="lazy"
+      allowFullScreen
+    >
+      See the Pen{' '}
+      <a href="https://codepen.io/dariadia/pen/bGWEzvP">Plot Generator</a> by
+      dariadia (<a href="https://codepen.io/dariadia">@dariadia</a>) on{' '}
+      <a href="https://codepen.io">CodePen</a>.
+    </iframe>
+  </>
+)
 
 export async function getStaticProps({
   locale,
